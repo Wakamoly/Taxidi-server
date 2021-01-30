c3171df5ceb98d3a20cdba7e20b15336711c03ff
Brendan Abbott <lucidsoftworksllc@gmail.com>
2021-01-30 11:30:01 -0500

    Oops

--- taxidi/DbConnect.php
+++ taxidi/DbConnect.php
@@ -9,7 +9,7 @@
 		}
 
 		function connect(){
-			include_once __DIR__ . 'Constants.php';
+			include_once __DIR__ . '/Constants.php';
 			//include_once 'config.php'; Delete config.php?
 			$this->con = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
